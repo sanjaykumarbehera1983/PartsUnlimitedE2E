26306ddeaf7865b76ec273894bcca13c9253742b
Sanjay Behera <sanjaykumarbehera1983@outlook.com>
2023-09-04 22:00:42 +0530

    Added Comment Second Commit Staged

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PartsUnlimited.Models
 {
-    //This is Third Change for Master Branch GitHub from VSsCode
+    //This is Third Changes for Master Branch GitHub from VSsCode
     public class Category
     {
         public int CategoryId { get; set; }
